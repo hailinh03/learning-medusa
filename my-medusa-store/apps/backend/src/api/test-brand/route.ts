95459127590c421f9c64aa18570de854d08b68c8
linh-ca-nhan <hailinh2910203@gmail.com>
2026-06-25 18:19:10 +0700

    feat: add test-brand API route and replace Elasticsearch with Meilisearch in docker-compose

---
+++ my-medusa-store/apps/backend/src/api/test-brand/route.ts
@@ -0,0 +1,17 @@
+import {
+  MedusaRequest,
+  MedusaResponse,
+} from "@medusajs/framework/http"
+import { createBrandWorkflow } from "../../workflows/create-brand"
+
+export const POST = async (
+  req: MedusaRequest,
+  res: MedusaResponse
+) => {
+  const { result } = await createBrandWorkflow(req.scope).run({
+    input: {
+      name: (req.body as any).name || "Test Brand Meilisearch",
+    },
+  })
+  res.json({ brand: result })
+}
