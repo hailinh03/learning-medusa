4021118c1591b17edb9bad8540cba6103c1b6450
linh-ca-nhan <hailinh2910203@gmail.com>
2026-06-26 18:33:58 +0700

    feat: integrate S3 file storage provider and add side navigation menu to storefront

--- my-medusa-store/apps/storefront/src/modules/layout/templates/nav/index.tsx
+++ my-medusa-store/apps/storefront/src/modules/layout/templates/nav/index.tsx
@@ -7,6 +7,8 @@ import { StoreRegion } from "@medusajs/types"
 import LocalizedClientLink from "@modules/common/components/localized-client-link"
 import CartButton from "@modules/layout/components/cart-button"
 
+import SideMenu from "@modules/layout/components/side-menu"
+
 export default async function Nav() {
   const [regions, locales, currentLocale] = await Promise.all([
     listRegions().then((regions: StoreRegion[]) => regions),
@@ -19,6 +21,7 @@ export default async function Nav() {
       <header className="relative h-16 mx-auto border-b duration-200 bg-white border-ui-border-base">
         <nav className="content-container txt-xsmall-plus text-ui-fg-subtle flex items-center justify-between w-full h-full text-small-regular">
           <div className="flex-1 basis-0 h-full flex items-center">
+            <SideMenu regions={regions} locales={locales} currentLocale={currentLocale} />
           </div>
 
           <div className="flex items-center h-full">
